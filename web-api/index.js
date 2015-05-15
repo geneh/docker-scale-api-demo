966952e41538e9e9a9ad489cf53e13d174bfd453
Gene H <ghazan@costco.com>
2015-05-15 15:11:34 -0700

    Initial commit

---
+++ web-api/index.js
@@ -0,0 +1,26 @@
+#!/bin/env node 127.0.0.1
+
+const express = require('express'),
+    hostname = require('os').hostname(),
+    redis = require('redis');
+
+const app = express(),
+    host = process.argv[2] || '127.0.0.1', //Redis host
+    client = redis.createClient(6379, host);
+
+// curl -H "Content-Type:application/json" http://localhost:3000/api/status
+app.get('/api/status', function(req, res) {
+    var payload = {};
+    payload.hostname = hostname;
+    payload.timestamp = Date.now();
+    client.incr(hostname);
+    res.json(payload);
+});
+
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://%s:%s', server.address().address, server.address().port);
+});
+
+client.on('error', function (err) {
+    console.log('Redis error', err);
+});
